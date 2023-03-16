5138ebef273648b1515e2585499c2003c4732d3e
bhenk <bhenk@telfortglasvezel.nl>
2023-03-16 15:50:53 +0100

    build phar

---
+++ docs/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Bootstrap used by doc2rst.
+ */
+$vendor_autoload = dirname(__DIR__)
+    . DIRECTORY_SEPARATOR . "vendor"
+    . DIRECTORY_SEPARATOR . "autoload.php";
+
+require_once $vendor_autoload;
---
+++ docs/build_docs.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd ../ || exit 1
+./doc2rst.phar
+sphinx-build -b html ./docs .docs/_build
